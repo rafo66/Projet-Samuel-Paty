8f64b087d7849c46b964c9ba2fbf5f52ee4e0da7
mapfire <nils.aym@gmail.com>
2023-02-02 13:36:12 +0100

    Last update

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from '../../components/navbar/Navigation';
 import Footer from '../../components/footer/Footer';
 import ArrowUp from '../../components/arrow-up/ArrowUp';
+import background from '../../assets/img/home-background.png';
 
 const Home = () => {
     return (
@@ -9,12 +10,28 @@ const Home = () => {
             <Navigation />
             <ArrowUp />
             <div className="content">
-                <div className='grid'>
-                    <div className='element' id="element1"><h1>Titre 1</h1></div>
-                    <div className='element' id="element2"><h1>Titre 2</h1></div>
-                    <div className='element' id="element3"><h1>Titre 3</h1></div>
-                    <div className='element' id="element4"><h1>Titre 4</h1></div>
+                <div className='title-container'>
+                    <img id="image" src={background} alt="arrière plan" />
+                    <div className='title'><h1>« Les infox : quels dangers pour la démocratie ? »</h1></div>
                 </div>
+                <div className='grid'>
+                    <div className='element' id="element1">
+                        <div className='content'>
+                            <h2>Infox</h2>
+                            <p>L'infox (ou désinformation) est de l'information erronée ou trompeuse diffusée intentionnellement ou non pour influencer les opinions ou les croyances d'un individu ou d'un groupe de personnes. Cela peut être fait par négligence, pour tromper ou pour manipuler l'opinion publique. Il est important de faire la distinction entre l'infox et l'information véridique pour maintenir une société bien informée.</p>
+                        </div>
+                    </div>
+                    <div className='element' id="element2"><h2>Titre 2</h2></div>
+                    <div className='element' id="element3">
+                        <div className='content'>
+                            <h2>Démocratie</h2>
+                            <p>La démocratie est un système politique dans lequel le pouvoir est exercé par le peuple, soit directement, soit par l'intermédiaire de représentants élus. Le terme "démocratie" vient du grec ancien "démokratia", composé des mots "dêmos" signifiant "peuple" et "kratos" signifiant "pouvoir".</p>
+                        </div>
+                    </div>
+                    <div className='element' id="element4"><h2>Titre 4</h2></div>
+
+                </div>
+
             </div>
             <Footer />
         </div>
